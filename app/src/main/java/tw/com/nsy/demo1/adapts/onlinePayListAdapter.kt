5730621b48d19856e14eeaa412e60063a8ec9454
neo.cheng <neo.cheng@tac.com.tw>
2021-08-09 17:31:56 +0800

    002

--- app/src/main/java/tw/com/nsy/demo1/adapts/onlinePayListAdapter.kt
+++ app/src/main/java/tw/com/nsy/demo1/adapts/onlinePayListAdapter.kt
@@ -8,14 +8,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import tw.com.nsy.demo1.R
+import tw.com.nsy.demo1.connectNetFunc.dataModel.ShopCarModel
 
 
 class onlinePayListAdapter(
     val ctx: Activity,
-    val caseList: ArrayList<LoanInfo>
+    val caseList: ShopCarModel
 ) : RecyclerView.Adapter<onlinePayListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val v = LayoutInflater.from(p0?.context).inflate(R.layout.uc_item_choose_car, p0, false)
+        val v = LayoutInflater.from(p0?.context).inflate(R.layout.item_shop_car, p0, false)
         return ViewHolder(v)
     }
 
@@ -30,37 +33,42 @@ class onlinePayListAdapter(
     }
 
     override fun getItemCount(): Int {
-        return caseList.size
+        return caseList.data.size
     }
 
-    fun getItem(position: Int): String {
-        return caseList.get(position).carNo
-    }
-
-    fun getItemloanNo(position: Int): String {
-        return caseList.get(position).loanNo
-    }
+//    fun getItem(position: Int): String {
+//        return caseList.data.get(position).carNo
+//    }
+//
+//    fun getItemloanNo(position: Int): String {
+//        return caseList.data.get(position).loanNo
+//    }
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        p0.tv_car_title.text = caseList[p1].carNo
 
+        p0.tv_shopcat_title.text = caseList.data[p1].martShortName
+        p0.tv_shopcat_money.text = caseList.data[p1].price.toString()
+
+        Glide.with(ctx)
+            .load(caseList.data[p1].imageUrl)
+            .into( p0.img_shopcar_photo);
     }
 
     //獲得事件回撥介面
     var item: ItemClick? = null;
 
     open inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tv_car_title = itemView.findViewById<TextView>(R.id.tv_car_title)!!
-        val img_car_choose = itemView.findViewById<ImageView>(R.id.img_car_choose)!!
+        val tv_shopcat_title = itemView.findViewById<TextView>(R.id.tv_shopcat_title)!!
+        val tv_shopcat_money = itemView.findViewById<TextView>(R.id.tv_shopcat_money)!!
+        val img_shopcar_photo = itemView.findViewById<ImageView>(R.id.img_shopcar_photo)!!
 
-        init {
-            itemView.setOnClickListener(View.OnClickListener {
-                if (itemClickListener != null) {
-                    itemClickListener.onItemClick(getItemloanNo(adapterPosition))
-                }
-                img_car_choose.background =
-                    ctx.resources.getDrawable(R.drawable.single_choice_press, null)
-            })
-        }
+//        init {
+//            itemView.setOnClickListener(View.OnClickListener {
+//                if (itemClickListener != null) {
+//                    itemClickListener.onItemClick(getItemloanNo(adapterPosition))
+//                }
+//
+//            })
+//        }
     }
 }
